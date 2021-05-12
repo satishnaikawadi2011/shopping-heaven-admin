a393e321057a12f6432a1800f6ed56367fdfbb84
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-05-12 21:42:56 +0530

    Successfully completed LayoutWrapper component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
 import React from 'react';
 import './App.css';
-import SHAppBar from './components/layout/SHAppBar';
-import AppDrawer from './components/layout/AppDrawer';
+import LayoutWrapper from './components/layout/LayoutWrapper';
 
 function App() {
 	return (
-		<React.Fragment>
-			<SHAppBar />
-			<AppDrawer />
-		</React.Fragment>
+		<LayoutWrapper>
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta odio dicta eius repellendus temporibus
+			delectus debitis amet, veniam harum ad consectetur, veritatis reprehenderit consequuntur rem cupiditate sint
+			tempore eligendi, corrupti sed itaque similique. Totam deserunt dolores, delectus vitae quibusdam quam
+			repudiandae, repellat quos aut omnis tempora accusamus maiores labore ex alias fugit at voluptatem in dolore
+			asperiores, quidem laudantium eveniet? Magni tempore expedita at facilis deleniti, ut nam hic, officia ipsa
+			corrupti, perferendis eos nobis eum! Sed dicta inventore eaque in minima consequuntur aliquam necessitatibus
+			sit odit quas? Minima consequuntur iure modi et distinctio. Debitis qui voluptatum, quae sint eligendi
+			laborum incidunt commodi voluptate! Ducimus rem corrupti unde amet nemo omnis qui nam quasi tenetur atque
+			reprehenderit officia odio odit beatae ratione ipsam, cumque iure architecto hic ut. Fuga reiciendis
+			architecto, vel explicabo placeat harum eveniet omnis sunt deleniti odit ad, amet quasi. Assumenda quia,
+			impedit nostrum maiores eligendi pariatur harum veritatis corrupti, veniam dolorum excepturi accusantium sed
+			nisi quidem ullam ipsam asperiores quisquam rem soluta repudiandae voluptas in cupiditate fugit numquam!
+			Consequatur magni nulla aperiam assumenda. Rem, totam dolores amet nobis sint corporis labore? Cum facere
+			alias veniam voluptas! Reiciendis ipsa commodi tempore explicabo ea, totam saepe, et optio laboriosam sint
+			sed sunt nulla incidunt expedita iure nisi ipsum iste repudiandae distinctio ratione corrupti voluptates.
+			Fugiat ipsa ratione, amet esse dolorum dolore saepe placeat a ab, officia et sapiente debitis nam, quasi
+			repellat consequuntur consequatur quod aliquid illum. Corporis porro, officia sunt, quis incidunt officiis
+			doloribus facere, est quasi ut cum reprehenderit soluta nobis quisquam dolores accusantium magni odio
+			deserunt commodi! Sapiente, expedita nihil! Repellat obcaecati praesentium ex sunt repudiandae! Magnam id
+			deleniti eveniet quas mollitia aperiam facilis, suscipit similique tenetur, sapiente amet? Magnam fuga
+			aperiam tempore. Distinctio numquam voluptate cum aliquam veniam excepturi est odio eius quam sequi non
+			consequuntur itaque ullam aspernatur maiores, delectus omnis soluta laudantium possimus aut? Suscipit vero
+			laudantium earum excepturi quam dolorum nam totam vel. A aut magnam velit modi qui adipisci, at sit laborum
+			alias rem hic quisquam voluptates eaque aperiam illum blanditiis molestiae ipsa est! Nam, laudantium quam
+			obcaecati omnis non esse. Assumenda consequatur voluptates excepturi! Animi dolor nisi dignissimos adipisci
+			quidem impedit quaerat explicabo enim dolorum aperiam aut dicta eius, minima harum odio praesentium
+			sapiente, vero ullam facilis temporibus. Iusto excepturi earum est beatae a numquam at alias asperiores
+			nesciunt provident eveniet repellat nulla facere ipsa qui commodi harum nam voluptatem assumenda, laudantium
+			ad vitae blanditiis sunt. Doloribus explicabo, odit neque fuga aperiam veritatis? Ex, consequuntur
+			necessitatibus repellat vel quo quos ratione minus iusto sunt aliquam facilis at. Voluptatem pariatur quae
+			alias at ea id reiciendis excepturi voluptate nostrum natus? Iure corporis magnam alias minima, repellendus,
+			accusantium praesentium illo quas commodi quia recusandae. Porro consequatur distinctio delectus repellat
+			similique possimus facere minus temporibus dicta, incidunt est unde in, odio dolore? Ducimus id, ut animi
+			qui suscipit atque, inventore corporis adipisci incidunt architecto ab earum modi quos consequuntur debitis
+			excepturi illo numquam quasi, quaerat reiciendis fuga natus. Nobis nisi molestiae porro!
+		</LayoutWrapper>
 	);
 }
 
---
+++ src/components/layout/LayoutWrapper.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import SHAppBar from './SHAppBar';
+import AppDrawer from './AppDrawer';
+
+interface LayoutProps {}
+
+const useStyles = makeStyles((theme: Theme) =>
+	createStyles({
+		root:
+			{
+				display: 'flex'
+			},
+		toolbar:
+			{
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'flex-end',
+				padding: theme.spacing(0, 1),
+				// necessary for content to be below app bar
+				...theme.mixins.toolbar
+			},
+		content:
+			{
+				flexGrow: 1,
+				padding: theme.spacing(3)
+			}
+	})
+);
+
+const LayoutWrapper: React.FC<LayoutProps> = ({ children }) => {
+	const classes = useStyles();
+	return (
+		<div className={classes.root}>
+			<SHAppBar />
+			<AppDrawer />
+			<main className={classes.content}>
+				<div className={classes.toolbar} />
+				{children}
+			</main>
+		</div>
+	);
+};
+
+export default LayoutWrapper;
