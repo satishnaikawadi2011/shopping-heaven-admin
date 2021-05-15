5956858bd750bd7bbad838f18bcd60990c18c88b
Satish Naikawadi <satishnaikawadi2001@gmail.com>
2021-05-16 01:23:22 +0530

    Completed all oprations related to orders.

--- src/components/UI/OrderTable.tsx
+++ src/components/UI/OrderTable.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { withStyles, Theme, createStyles, makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
@@ -15,9 +15,12 @@ import { Chip, IconButton } from '@material-ui/core';
 import dayjs from 'dayjs';
 import relativeTime from 'dayjs/plugin/relativeTime';
 
-import { INDIAN_RUPEE_SIGN } from '../../constants';
+import { AUTH_TOKEN_FOR_DEVELOPMENT, INDIAN_RUPEE_SIGN } from '../../constants';
 import { Order } from '../../models/Order';
 import { useHistory } from 'react-router';
+import AppAlertDialog from '../AppAlertDialog';
+import { useOrderStore } from '../../store/orders';
+import ordersApi from '../../api/orders';
 
 dayjs.extend(relativeTime);
 
@@ -59,75 +62,119 @@ interface OrderTableProps {
 }
 
 const OrderTable: React.FC<OrderTableProps> = ({ orders }) => {
+	const { markOrderAsDelivered, markOrderAsPaid } = useOrderStore();
 	const classes = useStyles();
 	const history = useHistory();
+	const [isPaying, setIsPaying] = useState(false);
+	const [
+		order,
+		setOrder
+	] = useState<Order>();
+	const [
+		openAlert,
+		setOpenAlert
+	] = useState(false);
+	const handleCloseAlert = () => {
+		setOpenAlert(false);
+	};
+	const handleDelivered = () => {
+		markOrderAsDelivered(order?._id as any);
+		ordersApi.markAsDelivered(AUTH_TOKEN_FOR_DEVELOPMENT, order?._id as any);
+		handleCloseAlert();
+	};
+		const handlePaid = () => {
+		markOrderAsPaid(order?._id as any);
+		ordersApi.markOrderAsPaid(AUTH_TOKEN_FOR_DEVELOPMENT, order?._id as any,'inr',order!.totalPrice);
+		handleCloseAlert();
+	};
 	return (
-		<TableContainer component={Paper}>
-			<Table className={classes.table} aria-label="customized table">
-				<TableHead>
-					<TableRow>
-						<StyledTableCell>ID</StyledTableCell>
-						<StyledTableCell align="right">Total Amount ({INDIAN_RUPEE_SIGN})</StyledTableCell>
-						<StyledTableCell align="right">Checkout on</StyledTableCell>
-						<StyledTableCell align="right">Delivery Status</StyledTableCell>
-						<StyledTableCell align="right">Payment Status</StyledTableCell>
-						<StyledTableCell align="center">Actions</StyledTableCell>
-					</TableRow>
-				</TableHead>
-				<TableBody>
-					{orders.map((order) => (
-						<StyledTableRow key={order._id}>
-							<StyledTableCell component="th" scope="row">
-								{order._id}
-							</StyledTableCell>
-							<StyledTableCell align="right">{order.totalPrice}</StyledTableCell>
-							<StyledTableCell align="right">{dayjs(order.createdAt).fromNow()}</StyledTableCell>
-							<StyledTableCell align="right">
-								{
-									order.isDelivered ? <Chip
-										style={{ backgroundColor: 'green', color: 'white' }}
-										label="Delivered"
-									/> :
-									<Chip
-										style={{ backgroundColor: 'yellow', color: 'black' }}
-										label="Not Delivered"
-									/>}
-							</StyledTableCell>
-							<StyledTableCell align="right">
-								{
-									order.isPaid ? <Chip
-										style={{ backgroundColor: 'green', color: 'white' }}
-										label="Paid"
-									/> :
-									<Chip style={{ backgroundColor: 'yellow', color: 'black' }} label="Pending" />}
-							</StyledTableCell>
-							<StyledTableCell align="center">
-								<Tooltip title="Mark As Delivered">
-									<IconButton aria-label="edit" disabled={order.isDelivered}>
-										<LocalShippingIcon style={{ color: 'green' }} />
-									</IconButton>
-								</Tooltip>
-								<Tooltip title="Mark As Paid">
-									<IconButton aria-label="edit" disabled={order.isPaid}>
-										<PaymentIcon style={{ color: 'green' }} />
-									</IconButton>
-								</Tooltip>
-								<Tooltip title="Details">
-									<IconButton
-										aria-label="details"
-										onClick={() => {
-											history.push(`/orders/${order._id}`);
-										}}
-									>
-										<VisibilityIcon style={{ color: 'skyblue' }} />
-									</IconButton>
-								</Tooltip>
-							</StyledTableCell>
-						</StyledTableRow>
-					))}
-				</TableBody>
-			</Table>
-		</TableContainer>
+		<React.Fragment>
+			<AppAlertDialog
+				description={`Are you sure you want to mark this order as ${isPaying ? 'paid':'delivered'} ?`}
+				handleClose={handleCloseAlert}
+				open={openAlert}
+				title={`Mark Order As ${isPaying ? 'Paid':'Delivered'}`}
+				onAgree={isPaying ? handlePaid:handleDelivered}
+			/>
+			<TableContainer component={Paper}>
+				<Table className={classes.table} aria-label="customized table">
+					<TableHead>
+						<TableRow>
+							<StyledTableCell>ID</StyledTableCell>
+							<StyledTableCell align="right">Total Amount ({INDIAN_RUPEE_SIGN})</StyledTableCell>
+							<StyledTableCell align="right">Checkout on</StyledTableCell>
+							<StyledTableCell align="right">Delivery Status</StyledTableCell>
+							<StyledTableCell align="right">Payment Status</StyledTableCell>
+							<StyledTableCell align="center">Actions</StyledTableCell>
+						</TableRow>
+					</TableHead>
+					<TableBody>
+						{orders.map((order) => (
+							<StyledTableRow key={order._id}>
+								<StyledTableCell component="th" scope="row">
+									{order._id}
+								</StyledTableCell>
+								<StyledTableCell align="right">{order.totalPrice}</StyledTableCell>
+								<StyledTableCell align="right">{dayjs(order.createdAt).fromNow()}</StyledTableCell>
+								<StyledTableCell align="right">
+									{
+										order.isDelivered ? <Chip
+											style={{ backgroundColor: 'green', color: 'white' }}
+											label="Delivered"
+										/> :
+										<Chip
+											style={{ backgroundColor: 'yellow', color: 'black' }}
+											label="Not Delivered"
+										/>}
+								</StyledTableCell>
+								<StyledTableCell align="right">
+									{
+										order.isPaid ? <Chip
+											style={{ backgroundColor: 'green', color: 'white' }}
+											label="Paid"
+										/> :
+										<Chip style={{ backgroundColor: 'yellow', color: 'black' }} label="Pending" />}
+								</StyledTableCell>
+								<StyledTableCell align="center">
+									<Tooltip title="Mark As Delivered">
+										<IconButton
+											aria-label="edit"
+											onClick={() => {
+												setIsPaying(false);
+												setOrder(order);
+												setOpenAlert(true);
+											}}
+											disabled={order.isDelivered}
+										>
+											<LocalShippingIcon style={{ color: 'green' }} />
+										</IconButton>
+									</Tooltip>
+									<Tooltip title="Mark As Paid">
+										<IconButton aria-label="edit" onClick={() => {
+												setIsPaying(true);
+												setOrder(order);
+												setOpenAlert(true);
+											}} disabled={order.isPaid}>
+											<PaymentIcon style={{ color: 'green' }} />
+										</IconButton>
+									</Tooltip>
+									<Tooltip title="Details">
+										<IconButton
+											aria-label="details"
+											onClick={() => {
+												history.push(`/orders/${order._id}`);
+											}}
+										>
+											<VisibilityIcon style={{ color: 'skyblue' }} />
+										</IconButton>
+									</Tooltip>
+								</StyledTableCell>
+							</StyledTableRow>
+						))}
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</React.Fragment>
 	);
 };
 
--- src/store/orders.ts
+++ src/store/orders.ts
@@ -4,9 +4,35 @@ import { Order } from '../models/Order';
 type OrderStore = {
 	orders: Order[] | null;
 	setOrders: (orders: Order[]) => void;
+	markOrderAsDelivered: (orderId: string) => void;
+	markOrderAsPaid: (orderId: string) => void;
 };
 
 export const useOrderStore = create<OrderStore>((set, get) => ({
 	orders: null,
-	setOrders: (orders) => set((state) => ({ ...state, orders }))
+	setOrders: (orders) => set((state) => ({ ...state, orders })),
+	markOrderAsDelivered: (orderId: string) => {
+		const updatedOrders:any = get().orders?.map((order) => {
+			if (order._id !== orderId) {
+				return order;
+			}
+			return {
+				...order,
+				isDelivered:true
+			}
+		})
+		set((state) => ({...state,orders:updatedOrders}))
+	},
+	markOrderAsPaid: (orderId: string) => {
+		const updatedOrders:any = get().orders?.map((order) => {
+			if (order._id !== orderId) {
+				return order;
+			}
+			return {
+				...order,
+				isPaid:true
+			}
+		})
+		set((state) => ({...state,orders:updatedOrders}))
+	}
 }));
